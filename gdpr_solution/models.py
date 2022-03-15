6a4ec9638be01994eeab446450552d4975dcf1b0
Lorenzo Monte <>
2022-03-15 11:49:12 +0100

    Update import if Wagtail project

--- gdpr_solution/models.py
+++ gdpr_solution/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel, RichTextField, MultiFieldPanel, HelpPanel
 from .app_config import COOKIEBANNER_IS_WAGTAIL_PROJECT, DJANGO_VERSION_TRANSLATION_UGETTEXT
 
 if DJANGO_VERSION_TRANSLATION_UGETTEXT:
     from django.utils.translation import ugettext_lazy as _
 else:
     from django.utils.translation import gettext_lazy as _
+
+if COOKIEBANNER_IS_WAGTAIL_PROJECT:
+    from wagtail.admin.edit_handlers import FieldPanel, RichTextField, MultiFieldPanel, HelpPanel
 
 
 COOKIE_TYPE_CHOICE = (
