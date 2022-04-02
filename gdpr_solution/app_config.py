7c798feabcd32d86cd82621b9d14836ff49d2ff5
Lorenzo <hellolorenzomonte@gmail.com>
2022-04-02 12:26:23 +0200

    Change minimum version of wagtail

--- gdpr_solution/app_config.py
+++ gdpr_solution/app_config.py
@@ -13,5 +13,5 @@ if parse_version(django.get_version()) < parse_version('2.0.0'):
 
 
 COOKIEBANNER_IS_WAGTAIL_PROJECT = False
-if WAGTAIL_VERSION and parse_version(WAGTAIL_VERSION) >= parse_version('1.13.4'):
+if WAGTAIL_VERSION and parse_version(WAGTAIL_VERSION) >= parse_version('2.0.2'):
     COOKIEBANNER_IS_WAGTAIL_PROJECT = True
