03eee4214bdcf83a76eb61462eda13552c6805f3
Lorenzo <hellolorenzomonte@gmail.com>
2022-08-30 09:04:16 +0200

    Clean package for new-start refactoring

--- gdpr_solution/app_config.py
+++ gdpr_solution/app_config.py
@@ -1,17 +1,1 @@
-import django
-from pkg_resources import parse_version
 
-try:
-    from wagtail import __version__ as WAGTAIL_VERSION
-except ImportError:
-    WAGTAIL_VERSION = False
-
-
-DJANGO_VERSION_LESS_THAN_2 = False
-if parse_version(django.get_version()) < parse_version('2.0.0'):
-    DJANGO_VERSION_LESS_THAN_2 = True
-
-
-COOKIEBANNER_IS_WAGTAIL_PROJECT = False
-if WAGTAIL_VERSION and parse_version(WAGTAIL_VERSION) >= parse_version('2.0.2'):
-    COOKIEBANNER_IS_WAGTAIL_PROJECT = True
